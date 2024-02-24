d1f90f7395e87f1d292369ee2051d3b648938304
Maël Gangloff <contact@maelgangloff.fr>
2024-02-24 17:54:16 +0100

    fix: remove old lines to fix a blocking error

--- dataV3.js
+++ dataV3.js
@@ -8,8 +8,6 @@ var json = JSON.parse(
 );
 
 var notes = json.results.filter(x => x.codeMatiere !== null);
-// console.log({notes});
-
 var username = document.getElementsByClassName("user")[0].children[0].innerText;
 
 var StringYUISorter, NumericYUISorter;
@@ -23,61 +21,6 @@ var json2 = eval(
         .replace(/(;var jsonArrayreleveEleve = ).*/m, "")
         // .replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ')
 );
-
-// --------------------- OLD THING  ---------------------
-var sum = 0.0;
-var averageSum = 0.0;
-var bestSum = 0.0;
-var worseSum = 0.0;
-var leng = 0;
-notes.forEach(element => {
-    if(element.moyenneEleve !== null) {
-        leng += 1;
-        sum = parseFloat(sum)+parseFloat(element.moyenneEleve.replaceAll(",", "."));
-        averageSum = parseFloat(averageSum)+parseFloat(element.moyenneClasse.replaceAll(",", "."));
-        bestSum = parseFloat(bestSum)+parseFloat(element.moyenneClasseMax.replaceAll(",", "."));
-        worseSum = parseFloat(worseSum)+parseFloat(element.moyenneClasseMin.replaceAll(",", "."));
-
-        // console.log(element.moyenneEleve.toString()+" : "+sum);
-    }
-});
-
-var avg = (sum / leng).toFixed(2);
-var best = (bestSum / leng).toFixed(2);
-var worse = (worseSum / leng).toFixed(2);
-var average = (averageSum / leng).toFixed(2);
-
-// PRINT THE AVERAGE
-
-/*
-var tr = document.createElement("tr");
-
-var td1 = "<td headers=\"yui-dt0-th-matiere \" class=\"yui-dt0-col-matiere yui-dt-col-matiere yui-dt-sortable yui-dt-first\" style=\"width: 150px;\"><div class=\"yui-dt-liner\"><div class=\"bulletin-matiere-ligne\">" +
-    "<div class=\"bulletin-matiere-libelle ellipse fw-700\" title=\"MOYENNE GENERALE\">Moyenne Générale</div>" +
-    "<div class=\"releve-matiere-professeur f-left c-both ellipse\" title=\""+username+"\">"+username +
-    "</div></div></div></td>";
-
-var td2 = "<td headers=\"yui-dt0-th-yui-dt-col1 yui-dt0-th-moyenneEleve \" class=\"yui-dt0-col-moyenneEleve yui-dt-col-moyenneEleve yui-dt-sortable\" style=\"width: 30px;\">" +
-    "<div class=\"yui-dt-liner bulletin-note bulletin-note-eleve\">"+avg.toString()+"</div></td>";
-
-var td3 = "<td headers=\"yui-dt0-th-yui-dt-col1 yui-dt0-th-nombreDevoirComptabilises \" class=\"yui-dt0-col-nombreDevoirComptabilises yui-dt-col-nombreDevoirComptabilises yui-dt-sortable\" style=\"width: 30px;\">" +
-    "<div class=\"yui-dt-liner bulletin-note\"><div class=\"txt-center\">"+leng+"</div></div></td>";
-
-var td4 = "<td headers=\"yui-dt0-th-yui-dt-col4 yui-dt0-th-moyenneClasse \" class=\"yui-dt0-col-moyenneClasse yui-dt-col-moyenneClasse\" style=\"width: 30px;\">" +
-    "<div class=\"yui-dt-liner bulletin-note\">"+average+"</div></td>";
-
-var td5 = "<td headers=\"yui-dt0-th-yui-dt-col4 yui-dt0-th-moyenneClasseMin \" class=\"yui-dt0-col-moyenneClasseMin yui-dt-col-moyenneClasseMin\" style=\"width: 30px;\">" +
-    "<div class=\"yui-dt-liner bulletin-note\">"+worse+"</div></td>";
-
-var td6 = "<td headers=\"yui-dt0-th-yui-dt-col4 yui-dt0-th-moyenneClasseMax \" class=\"yui-dt0-col-moyenneClasseMax yui-dt-col-moyenneClasseMax\" style=\"width: 30px;\">" +
-    "<div class=\"yui-dt-liner bulletin-note\">"+best+"</div></td>";
-
-tr.innerHTML=td1+td2+td3+td3+td5+td6;
-
-document.getElementById("releve-eleve").children[1].children[3].appendChild(tr);
-*/
-
-// --------------------- NEW SYSTEM ---------------------
 
 var noteArray = {};
 
